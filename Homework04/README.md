b13478b0531a962850b06e7bfc5164294180a965
Руслан <32477962+Xrustik@users.noreply.github.com>
2018-12-03 21:25:28 +0200

    Add files via upload

---
+++ Homework04/README.md
@@ -0,0 +1,1 @@
+http://www.codeskulptor.org/#user45_ibw8Ukrg9y_1.py
---
+++ Homework04/index.py
@@ -0,0 +1,65 @@
+import simplegui
+
+sec = 0
+minute = 0
+message = "0:00.0"
+
+accounts = "0/0"
+victory = 0
+defeat = 0
+
+def start():
+    global message
+    global sec
+    global minute
+    timer.start()
+    sec += 0.1
+    if sec >59.9:
+        minute += 1
+        sec = 0
+    if sec > 10:
+        message = str(minute)+":"+str(sec)
+    else:
+        message = str(minute)+":0"+str(sec)
+        
+def stop():
+    timer.stop()
+    convert(sec)
+    
+def convert(val):
+    global victory
+    global defeat
+    global accounts
+    accounts = int(val)
+    msec = int(round(10 * (val - accounts)))
+    
+    if msec == 0:
+        victory += 1
+    else:
+        defeat += 1
+    accounts = str(victory)+"/"+str(defeat)
+    
+def remov():
+    global message,minute,sec
+    global accounts,victory,defeat
+    sec = 0
+    minute = 0
+    message = "0:00.0"
+    
+    accounts = "0/0"
+    victory = 0
+    defeat = 0
+    
+
+def draw(canvas):
+    canvas.draw_text(message, [50,120], 48, "Red")
+    canvas.draw_text(accounts, [160,20], 20, "Red")
+
+frame = simplegui.create_frame("Home", 200, 200)
+frame.add_button("Start", start)
+frame.add_button("Stop", stop)
+frame.add_button("Remov", remov)
+frame.set_draw_handler(draw)
+timer = simplegui.create_timer(100, start)
+
+frame.start()
