ea3f483fc939bb901fc6d0aee04e021a6330c06e
Руслан <32477962+Xrustik@users.noreply.github.com>
2018-12-03 21:26:44 +0200

    Update index.py

--- Homework04/index.py
+++ Homework04/index.py
@@ -1,65 +1,126 @@
 import simplegui
+import random
 
-sec = 0
-minute = 0
-message = "0:00.0"
 
-accounts = "0/0"
-victory = 0
-defeat = 0
+WIDTH = 600
+HEIGHT = 400       
+BALL_RADIUS = 20
+PAD_WIDTH = 8
+PAD_HEIGHT = 80
+HALF_PAD_WIDTH = PAD_WIDTH / 2
+HALF_PAD_HEIGHT = PAD_HEIGHT / 2
+ball_pos = [WIDTH/2,HEIGHT/2]
+ball_vel = [-random.randrange(60, 180)/50,random.randrange(110, 240)/50]
+paddle1_pos = HEIGHT/2
+paddle2_pos = HEIGHT/2
 
-def start():
-    global message
-    global sec
-    global minute
-    timer.start()
-    sec += 0.1
-    if sec >59.9:
-        minute += 1
-        sec = 0
-    if sec > 10:
-        message = str(minute)+":"+str(sec)
+
+
+def ball_in_game(right):
+    global ball_pos, ball_vel 
+    ball_pos = [WIDTH/2,HEIGHT/2]
+    ball_vel[1] = -random.randrange(60, 180)/50
+    if right == True:
+        ball_vel[0] = random.randrange(120, 240)/50
     else:
-        message = str(minute)+":0"+str(sec)
-        
-def stop():
-    timer.stop()
-    convert(sec)
-    
-def convert(val):
-    global victory
-    global defeat
-    global accounts
-    accounts = int(val)
-    msec = int(round(10 * (val - accounts)))
-    
-    if msec == 0:
-        victory += 1
-    else:
-        defeat += 1
-    accounts = str(victory)+"/"+str(defeat)
-    
-def remov():
-    global message,minute,sec
-    global accounts,victory,defeat
-    sec = 0
-    minute = 0
-    message = "0:00.0"
-    
-    accounts = "0/0"
-    victory = 0
-    defeat = 0
-    
+        ball_vel[0] = -random.randrange(120, 240)/50
+    pass
+
+# define event handlers
+def new_game():
+    global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel 
+    global score1, score2  
+    paddle1_pos = HEIGHT/2
+    paddle2_pos = HEIGHT/2
+    paddle1_vel = 0
+    paddle2_vel = 0
+    score1 = 0
+    score2 = 0
+    ball_in_game(0 == random.randrange(0,11) % 2)
+    pass
 
 def draw(canvas):
-    canvas.draw_text(message, [50,120], 48, "Red")
-    canvas.draw_text(accounts, [160,20], 20, "Red")
+    global score1, score2
+    global paddle1_vel, paddle2_vel
+    global paddle1_pos, paddle2_pos
+    global ball_pos, ball_vel
+    
+    if paddle1_pos < (HALF_PAD_HEIGHT) and paddle1_vel < 0:
+        paddle1_vel = 0
+    if paddle2_pos < (HALF_PAD_HEIGHT) and paddle2_vel < 0:
+        paddle2_vel = 0
+    if paddle1_pos > (HEIGHT - (HALF_PAD_HEIGHT)) and paddle1_vel > 0:
+        paddle1_vel = 0
+    if paddle2_pos > (HEIGHT - (HALF_PAD_HEIGHT)) and paddle2_vel > 0:
+        paddle2_vel = 0    
+    paddle1_pos += paddle1_vel
+    paddle2_pos += paddle2_vel        
 
-frame = simplegui.create_frame("Home", 200, 200)
-frame.add_button("Start", start)
-frame.add_button("Stop", stop)
-frame.add_button("Remov", remov)
+    
+    ball_pos[0] += ball_vel[0]
+    ball_pos[1] += ball_vel[1]
+    
+    if ball_pos[1] >= (HEIGHT - BALL_RADIUS) or ball_pos[1] <= (BALL_RADIUS):
+        ball_vel[1] = -ball_vel[1]
+    if ball_pos[0] <= (PAD_WIDTH + BALL_RADIUS):
+        if ball_pos[1] < (paddle1_pos - HALF_PAD_HEIGHT) or ball_pos[1] > (paddle1_pos + HALF_PAD_HEIGHT):
+            ball_in_game(True)
+            score2 += 1
+        else:
+            ball_vel[0] = -ball_vel[0] * 1.1
+            
+    if  ball_pos[0] >= (WIDTH - PAD_WIDTH - BALL_RADIUS):
+        if ball_pos[1] < (paddle2_pos - HALF_PAD_HEIGHT) or ball_pos[1] > (paddle2_pos + HALF_PAD_HEIGHT):
+            ball_in_game(False)
+            score1 += 1
+        else:
+            ball_vel[0] = -ball_vel[0] * 1.1
+    
+    
+    # draw mid line and gutters
+    canvas.draw_line([WIDTH / 2, 0],[WIDTH / 2, HEIGHT], 1, "White")
+    canvas.draw_line([PAD_WIDTH, 0],[PAD_WIDTH, HEIGHT], 1, "White")
+    canvas.draw_line([WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1, "White")
+    # draw paddles
+    canvas.draw_polygon([(0, paddle1_pos-HALF_PAD_HEIGHT), (0, paddle1_pos+HALF_PAD_HEIGHT), (PAD_WIDTH-2, paddle1_pos+HALF_PAD_HEIGHT),(PAD_WIDTH-2,paddle1_pos-HALF_PAD_HEIGHT)], PAD_WIDTH-1, "White","White")
+    canvas.draw_polygon([(WIDTH, paddle2_pos-HALF_PAD_HEIGHT), (WIDTH, paddle2_pos+HALF_PAD_HEIGHT), (WIDTH-PAD_WIDTH+2, paddle2_pos+HALF_PAD_HEIGHT),(WIDTH-PAD_WIDTH+2,paddle2_pos-HALF_PAD_HEIGHT)], PAD_WIDTH-1, "White","White")
+    
+    
+    canvas.draw_circle(ball_pos, BALL_RADIUS, 2, "Red", "White")
+    canvas.draw_text(str(score1), (170, 50), 36, "Red")
+    canvas.draw_text(str(score2), (400, 50), 36, "Red")
+        
+def keydown(key):
+    global paddle1_vel, paddle2_vel
+    
+    if key == simplegui.KEY_MAP['w']:
+        paddle1_vel = -6
+    elif key == simplegui.KEY_MAP['s']:
+        paddle1_vel = 6
+    elif key == simplegui.KEY_MAP['up']:
+        paddle2_vel = -6
+    elif key == simplegui.KEY_MAP['down']:
+        paddle2_vel = 6
+   
+def keyup(key):
+    global paddle1_vel, paddle2_vel
+
+    if key == simplegui.KEY_MAP['w']:
+        paddle1_vel = 0
+    elif key == simplegui.KEY_MAP['s']:
+        paddle1_vel = 0
+    elif key == simplegui.KEY_MAP['up']:
+        paddle2_vel = 0
+    elif key == simplegui.KEY_MAP['down']:
+        paddle2_vel = 0
+
+
+frame = simplegui.create_frame("Pong", WIDTH, HEIGHT)
 frame.set_draw_handler(draw)
-timer = simplegui.create_timer(100, start)
+frame.set_keydown_handler(keydown)
+frame.set_keyup_handler(keyup)
+frame.add_button("Restart", new_game, 100)
 
+
+new_game()
 frame.start()
